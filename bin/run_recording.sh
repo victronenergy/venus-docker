6ae1f26fb7878162cbc4b4243002f6e39b4ac43c
Heikki Honkanen <heikki.honkanen@reaktor.com>
2018-12-12 14:35:04 +0100

    Change recorder path in run_recording.sh

--- bin/run_recording.sh
+++ bin/run_recording.sh
@@ -3,9 +3,9 @@
 # Use this to run killed/new recordings in a running container
 
 if [ $# -eq 0 ]; then
-    echo "Usage: $0 <recording_file_name>. The recording file must exist in /root/dbus-recorder"
+    echo "Usage: $0 <recording_file_name>. The recording file must exist in /opt/victronenergy/dbus-recorder"
     echo "e.g. $0 vebus-marine.dat"
     exit 1
 fi
 
-python /root/dbus-recorder/dbusrecorder.py -p --file=/root/dbus-recorder/$1 &
+python /opt/victronenergy/dbus-recorder/dbusrecorder.py -p --file=/opt/victronenergy/dbus-recorder/$1 &
