6834ebd16f86323143beccb437b6e8b12d975058
Izak Burger <isburger@gmail.com>
2018-12-14 10:31:59 +0200

    Added dbus setup to each simulation

--- simulate.sh
+++ simulate.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 PLAY="/usr/bin/python /opt/victronenergy/dbus-recorder/play.py"
+DBUS="/usr/bin/dbus"
 
 help() {
 	echo "Usage: $0 simulation_name"
@@ -18,5 +19,13 @@ if test -z "$1"; then
 fi
 
 sim=${1,,}
+
+# Perform any setup that might be required for this demo
+if test -f simulations/$sim/setup; then
+	while read -r line; do
+		read -r service path value <<< "$line"
+		$DBUS -y $service $path SetValue $value > /dev/null
+	done < simulations/$sim/setup
+fi
 
 $PLAY simulations/$sim/*.dat
