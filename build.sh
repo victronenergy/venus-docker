1b029e02a0df6ffb8a11851e8e114f9c22afc980
Izak Burger <isburger@gmail.com>
2019-02-08 12:20:59 +0200

    Track latest versions of submodules one level deep
    When building the docker image, update the code to the latest
    revision (HEAD), but do not do the same for nested submodules
    to avoid using newer versions that might not be tested
    with this project yet.

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
-git submodule update --init --recursive
+# Update submodules to HEAD one level deep
+git submodule update --init --remote
+
+# For each submodule, update nested submodules (not to HEAD).
+git submodule foreach 'git submodule update --init'
 
 docker build . -t mqtt
