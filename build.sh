059acf5380de52c324f06dd992cfa4ea33cb4352
Heikki Honkanen <heikki.honkanen@reaktor.com>
2019-02-19 10:04:03 +0100

    Remove unnecessary submodule init

--- build.sh
+++ build.sh
@@ -4,7 +4,4 @@ git submodule update --init --remote
 # For each submodule, update nested submodules (not to HEAD).
 git submodule foreach 'git submodule update --init'
 
-# Update and build html5 app
-git submodule update --init --remote venus-html5-app
-
 docker build . -t mqtt
