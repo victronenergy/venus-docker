da5fdc3fdaaa5b7b394039b9dd8972bc31156d93
Martin Bosma <mbosma@victronenergy.com>
2023-10-02 12:14:44 +0200

    build.sh: stop on git failure

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Update submodules
-git submodule update --init --recursive
+git submodule update --init --recursive || exit
 
 # Update all the submodules and its dependencies
-git submodule foreach git pull --ff origin master --recurse-submodules
+git submodule foreach git pull --ff origin master --recurse-submodules || exit
 
 # Build a new mqtt Docker image
 docker build . -t mqtt --no-cache
